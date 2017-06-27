ff41f3c9f8b6ebbc96d27e0563684d9f882f3351
Kevin Peng <kevin.peng@btcc.com>
2017-06-27 12:55:23 +0800

    add file

---
+++ Example/MZImageView/MZManager.h
@@ -0,0 +1,13 @@
+//
+//  MZManager.h
+//  MZImageView
+//
+//  Created by Kevin Peng on 27/6/17.
+//  Copyright © 2017年 Pillian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MZManager : NSObject
+
+@end
---
+++ Example/MZImageView/MZManager.m
@@ -0,0 +1,13 @@
+//
+//  MZManager.m
+//  MZImageView
+//
+//  Created by Kevin Peng on 27/6/17.
+//  Copyright © 2017年 Pillian. All rights reserved.
+//
+
+#import "MZManager.h"
+
+@implementation MZManager
+
+@end
